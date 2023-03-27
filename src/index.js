77e5eb5a1877730dd27616bad3afb4ef360b74a2
Tetiana Novikova <113465579+NovikovaTetiana@users.noreply.github.com>
2023-03-28 00:11:50 +0300

    make task

--- src/index.js
+++ src/index.js
@@ -7,55 +7,81 @@ const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
 const ulEl = document.querySelector('.country-list');
+const divEl = document.querySelector('.country-info');
 
+inputEl.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
-inputEl.addEventListener('input', debounce(handleInputForm, DEBOUNCE_DELAY));
+function handleInput(e) {
+  const nameCountry = e.target.value.trim();
 
-function handleInputForm (event){
-
-  const nameCountry = event.target.value.trim();
-  console.log(nameCountry)
-
-    fetchCountries(nameCountry)
-    .then (data => {
-      console.log(data)
-    })
-    .catch (err => {
-      Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-    })
+  if (nameCountry) {
+    return fetchCountries(nameCountry)
+      .then(data => {
+        choseMarkup(data);
+      })
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+      });
   }
 
+  ulEl.innerHTML = '';
+  divEl.innerHTML = '';
+}
 
-  // function renderInfo({
-  //   name,
-  //   capital,
-  //   population,
-  //   flags,
-  //   languages,
-  // }) {
-  //   return `
-  //     <div class="info__header">
-  //   <img src="${flags.png}" alt="${flags.alt}" class="info__img" />
-  //   <h2 class="info__name">${name.official}</h2>
-  // </div>
-  // <ul class="info__list">
-  //   <li class="info__item">
-  //     <h4 class="item__key">Capital:</h4>
-  //     <span class="item__value">${capital}</span>
-  //   </li>
-  //   <li class="info__item">
-  //     <h4 class="item__key">Population:</h4>
-  //     <span class="item__value">${population}</span>
-  //   </li>
-  //   <li class="info__item">
-  //     <h4 class="item__key">Languages:</h4>
-  //     <span class="item__value">
-  //     <ul class="list__languages">
-  //    ${renderLanguages(languages)}
-  //     </ul>
-  //     </span>
-  //   </li>
-  // </ul>
-  //     `;
-  // }
+function choseMarkup(countries) {
+  if (countries.length === 1) {
+    ulEl.innerHTML = '';
+    return markupCountry(countries);
+  }
+  if (countries.length >= 2 && countries.length <= 10) {
+    divEl.innerHTML = '';
+    return markupList(countries);
+  }
 
+  return Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+}
+
+function markupList(data) {
+  const markup = data
+    .map(el => {
+      return `<li class="country-item">
+            <img src="${el.flags.svg}" alt="${el.name.official}" width="40" height="20" /> 
+            <p>${el.name.official}</p>
+            </li>`;
+    })
+    .join('');
+
+    ulEl.innerHTML = markup;
+}
+
+function markupCountry(data) {
+  const markup = data
+    .map(el => {
+      return `<h1>
+       <img src="${el.flags.svg}" alt="${
+        el.name.official
+      }" width="40" height="20" /> 
+            
+        ${el.name.official}
+      </h1>
+      <ul class="country-info_list">
+        <li class="country-info_item">
+          <h2>Capital:</h2>
+          <p>${el.capital}</p>
+        </li>
+        <li class="country-info_item">
+          <h2>Population:</h2>
+          <p>${el.population}</p>
+        </li>
+        <li class="country-info_item">
+          <h2>Languages:</h2>
+          <p>${Object.values(el.languages).join(', ')}</p>
+        </li>
+      </ul>`;
+    })
+    .join('');
+
+    divEl.innerHTML = markup;
+}
